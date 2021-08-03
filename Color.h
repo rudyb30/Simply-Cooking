a8904340cdafad082a5b41df7b2182f7c2d531c5
rudyb30 <73556466+rudyb30@users.noreply.github.com>
2021-08-02 20:51:57 -0400

    Add files via upload

---
+++ Color.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <iostream>
+#include <queue>
+using namespace std;
+
+enum Color { Red, Black }; // An enum is used here because we expect all nodes on a tree to be either Red or Black, and nothing else.
