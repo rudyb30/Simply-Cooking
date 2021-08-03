7c296e4f76d2f824fde96328f750d252f975ad91
rudyb30 <73556466+rudyb30@users.noreply.github.com>
2021-08-02 21:09:30 -0400

    Add files via upload

---
+++ Logic.hpp
@@ -0,0 +1,15 @@
+//  Created by Rudy on 7/4/21.
+
+#ifdef __cplusplus
+
+extern "C"{
+#endif
+//a prototype of all c++ functions that will be used in the swift interface must be placed here
+const char* getRecipes (const char * ingredientList, const char * allergyList);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
