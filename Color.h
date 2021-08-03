698c5e847fa28118debedb8036f7b60217ec3a1a
rudyb30 <73556466+rudyb30@users.noreply.github.com>
2021-08-02 22:26:47 -0400

    Delete Color.h

--- Color.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <iostream>
-#include <queue>
-using namespace std;
-
-enum Color { Red, Black }; // An enum is used here because we expect all nodes on a tree to be either Red or Black, and nothing else.
